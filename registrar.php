1136d138532637da06995c8ea1c8b1ae8edb0114
david <dsobped691a@ieslacampina.es>
2024-03-21 13:24:55 +0100

    ultima cambios

--- registrar.php
+++ registrar.php
@@ -18,37 +18,36 @@
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" href="estiloregistrar.css">
+		<link rel="stylesheet" href="estilos.css">
 		<link rel="shortcut icon" href="images/icono.png">
 		
-		<title>Agrupacion Santa Maria Magdalena</title>
-	</head>
-	<header>
-		
-	<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-  <h1 class="navbar-brand">Agrupacion Santa Maria Magdalena</h1>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+		<title>Agrupación Santa Maria Magdalena</title>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-	  <a class="nav-link" href="panel_de_control.php">Inicio <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Acciones
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="registrar.php">Registrar</a>
-          <a class="dropdown-item" href="#">Proximamente</a>
-        </div>
-      </li>
-    </ul>
-	<a href="login.php" class="form-inline my-2 my-lg-0 nav-link">Cerrar Sesion</a>
-  </div>
+<header>
+	
+<nav class="navbar navbar-expand-lg navbar black ">
+<img src="images/logo.jpg" alt="grande" style="width: 50px;" class="align-self-left">
+<h1><a class="nav-link" href="panel_de_control.php">Agrupación Santa Maria Magdalena</h1></a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+<ul class="navbar-nav ml-auto" >
+  <li class="nav-item dropdown">
+	<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Acciones</a>
+	<div class="dropdown-menu"  aria-labelledby="navbarDropdown">
+	  <a class="dropdown-item" href="registrar.php">Registrar nuevo musico</a>
+	  <a class="dropdown-item" href="contratos.php">Actuaciones</a>
+	</div>
+  </li>
+</ul>
+<a href="index.php" class="form-inline my-2 my-lg-0 nav-link">Cerrar Sesion</a>
+</div>
 </nav>
-	</header>
+</header>
+
+</head>
 	<body>
 		<div class="container">
 			<div class="row">
