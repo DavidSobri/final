13479a89fb670d8260894594a46e69a3b6ce0e7f
DavidSobri <davisobri78@gmail.com>
2024-03-22 15:40:56 +0100

    Contratos hecho junto boton estilo negrita

--- contratos.php
+++ contratos.php
@@ -1,65 +1,85 @@
 <?php
 require 'conexion.php';
+$sql = "SELECT * FROM contratos";
+$resultado = $mysqli->query($sql);
+
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/jquery.dataTables.min.css">
-	<link rel="stylesheet" href="estilos.css">
-	<link rel="shortcut icon" href="images/icono.png">
-
-	<!-- Optional JavaScript -->
-	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="js/jquery-3.4.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.dataTables.min.js"></script>
-
-	<title>Agrupacion Santa Maria Magdalena</title>
-
-	<!-- <script>
-		$(document).ready( function () {
-			$('#tabla').DataTable();
-		} );
-	</script> -->
-	<header>
+<!doctype html>
+<html lang="es">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		
-	<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-  <h1 class="navbar-brand">Agrupacion Santa Maria Magdalena</h1>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="estiloregistrar.css">
+		<link rel="stylesheet" href="estilos.css">
+		<link rel="shortcut icon" href="images/icono.png">
+		
+		<title>Agrupación Santa Maria Magdalena</title>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-	  <a class="nav-link" href="panel_de_control.php">Inicio <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Acciones
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="registrar.php">Registrar</a>
-          <a class="dropdown-item" href="error.php">Proximamente</a>
-        </div>
-      </li>
-    </ul>
-	<a href="error.php" class="form-inline my-2 my-lg-0 nav-link">Login</a>
-  </div>
+<header>
+	
+<nav class="navbar navbar-expand-lg navbar black ">
+<img src="images/logo.jpg" alt="grande" style="width: 50px;" class="align-self-left">
+<h1><a class="nav-link" href="panel_de_control.php">Agrupación Santa Maria Magdalena</h1></a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+<ul class="navbar-nav ml-auto" >
+  <li class="nav-item dropdown">
+	<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Acciones</a>
+	<div class="dropdown-menu"  aria-labelledby="navbarDropdown">
+	  <a class="dropdown-item" href="registrar.php">Registrar nuevo musico</a>
+	  <a class="dropdown-item" href="contratos.php">Actuaciones</a>
+	</div>
+  </li>
+</ul>
+<a href="index.php" class="form-inline my-2 my-lg-0 nav-link">Cerrar Sesion</a>
+</div>
 </nav>
-	</header>
+</header>
 
 </head>
 <body>
-    <div class="d-flex flex-column ">
-		<h1 class="align-self-center align-self-xl-center align-self-lg-center align-self-md-center align-self-sm-center" style="color:mediumspringgreen">OOPS! Vuelva mas tarde</h1>
-		<img src="images/ups.jpg" alt="grande" style="width: 35%;" class="align-self-center align-self-xl-center align-self-lg-center align-self-md-center align-self-sm-center">
+<div class="container-fluid ">
+		<!-- <div class="row">
+			<a class='btn btn-primary' href='registrar.php'>Registrar</a>
+		</div> -->
+		<div class="d-flex justify-content-center justify-content-sm-center justify-content-md-center justify-content-lg-center justify-content-xl-center" style="width:100%">
+		<div class="tabla alig-items-center " style="width:85%; margin: auto;">
+			<table id="tabla" class="table table-primary " style="width:100%">
+				<thead>
+				<tr class="table-black">
+						<th></th>
+						<th>Proximas Actuaciones</th>
+						<th></th>
+						<th></th>
+					</tr>
+					<tr class="table-primary">
+						<th>Contrato</th>
+						<th>Fecha</th>
+						<th></th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					while ($fila = $resultado->fetch_assoc()) {
+						echo "<tr class='table'>";
+						echo "<td>$fila[contrato]</td>";
+						echo "<td>$fila[fecha]</td>";
+						echo "<td><a class='btn btn-danger' href='eliminar.php?id=$fila[id_contrato]'>Eliminar</a></td>";
+						echo "</tr>";
+					}
+					?>
+				</tbody>
+			</table>
+			<a class='btn btn-warning' href='registrar_contrato.php' style='font-weight:bold;'>Nuevo contrato</a>
+		</div>
 </body>
 </html>
