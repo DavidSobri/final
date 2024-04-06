4928ec4ce0c985ab24d859a429394469f5aed5c8
DavidSobri <davisobri78@gmail.com>
2024-04-06 23:02:52 +0200

    los datosphp cambiados

--- datos.php
+++ datos.php
@@ -1,6 +1,7 @@
 <?php
 
 require 'conexion.php';
+//pillo los datos por su id
 $id = $_GET['id'];
 $sql2 = "SELECT * FROM musico where id=$id";
 $resultado2 = $mysqli->query($sql2);
@@ -22,7 +23,7 @@ $fila2=$resultado2->fetch_assoc();
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="estiloregistrar.css">
     <link rel="stylesheet" href="estilos.css">
-    <link rel="shortcut icon" href="images/icono.png">
+    <link rel="shortcut icon" href="images/logo.jpg">
 
     <title>Agrupación Santa Maria Magdalena</title>
 
@@ -30,24 +31,15 @@ $fila2=$resultado2->fetch_assoc();
 
         <nav class="navbar navbar-expand-lg navbar black ">
             <img src="images/logo.jpg" alt="grande" style="width: 50px;" class="align-self-left">
-            <h1><a class="nav-link" href="panel_de_control.php">Agrupación Santa Maria Magdalena</h1></a>
+            <h1><a class="nav-link" href="menu_usuario.php?id=<?php echo $id ?>">Agrupación Santa Maria Magdalena</h1></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Acciones</a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="registrar.php">Registrar nuevo musico</a>
-                            <a class="dropdown-item" href="contratos.php">Actuaciones</a>
-                        </div>
-                    </li>
-                </ul>
-                <a href="index.php" class="form-inline my-2 my-lg-0 nav-link">Cerrar Sesion</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent" style="justify-content:end">
+            <a href="contratos2.php" class="form-inline my-2 my-lg-0 nav-link">Actuaciones</a>
+            <a href="index.php" class="form-inline my-2 my-lg-0 nav-link">Cerrar Sesion</a>
             </div>
         </nav>
     </header>
@@ -56,9 +48,6 @@ $fila2=$resultado2->fetch_assoc();
 
 <body>
     <div class="container">
-        <div class="row" style="justify-content: center;">
-            <h1>Registrar musico</h1>
-        </div>
         <br><br>
         <div class="card">
             <div class="col-md-8">
--- datos2.php
+++ datos2.php
@@ -1,6 +1,7 @@
 <?php
 
 require 'conexion.php';
+//traigo sus datos
 $id = $_POST['id'];
 $sql2 = "SELECT * FROM musico where id=$id";
 $resultado2 = $mysqli->query($sql2);
@@ -22,7 +23,7 @@ $fila2=$resultado2->fetch_assoc();
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="estiloregistrar.css">
     <link rel="stylesheet" href="estilos.css">
-    <link rel="shortcut icon" href="images/icono.png">
+    <link rel="shortcut icon" href="images/logo.jpg">
 
     <title>Agrupación Santa Maria Magdalena</title>
 
@@ -30,27 +31,20 @@ $fila2=$resultado2->fetch_assoc();
 
         <nav class="navbar navbar-expand-lg navbar black ">
             <img src="images/logo.jpg" alt="grande" style="width: 50px;" class="align-self-left">
-            <h1><a class="nav-link" href="panel_de_control.php">Agrupación Santa Maria Magdalena</h1></a>
+            <h1><a class="nav-link" href="menu_usuario.php?id=<?php echo $id ?>">Agrupación Santa Maria Magdalena</h1></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Acciones</a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="registrar.php">Registrar nuevo musico</a>
-                            <a class="dropdown-item" href="contratos.php">Actuaciones</a>
-                        </div>
-                    </li>
-                </ul>
-                <a href="index.php" class="form-inline my-2 my-lg-0 nav-link">Cerrar Sesion</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent" style="justify-content:end">
+            <a href="contratos2.php" class="form-inline my-2 my-lg-0 nav-link">Actuaciones</a>
+            <a href="index.php" class="form-inline my-2 my-lg-0 nav-link">Cerrar Sesion</a>
             </div>
         </nav>
     </header>
+
+</head>
 
 </head>
 
@@ -63,14 +57,14 @@ $fila2=$resultado2->fetch_assoc();
 	$nombre =$_POST['nombre'];
     $apellido=$_POST['apellido'];
     $telefono=$_POST['telefono'];
-
+    //modifica
     $sql = "UPDATE musico SET nombre='$nombre', apellido='$apellido' , telefono='$telefono'  WHERE id=$id";
     //se ejecuta la sentencia y se gurada el resultado en resultado
     $resultado = $mysqli -> query($sql);
-
+    
 	if ($resultado > 0) {
 		echo "<div class='d-flex justify-content-center justify-content-sm-center justify-content-md-center justify-content-lg-center justify-content-xl-center' style='width:100%'>";
-    echo "<h3>Contrato aceptado</h3><br>";
+    echo "<h3>Datos modificados correctamente</h3><br>";
     echo "</div>";
     echo "<div class='d-flex justify-content-center justify-content-sm-center justify-content-md-center justify-content-lg-center justify-content-xl-center' style='width:100%'>";
     echo "<p><p><a href='menu_usuario.php?id=$fila2[id]' class='btn btn-primary'>Ir al inicio</a></p>";
